6830ab9881853e1edd43211f94c9d78b3f9b1785
Jeff Eickholt <jteickholt@gmail.com>
2020-02-22 13:27:49 -0600

    Uploading Latest Version of Files

--- static/py/app-weather_jeff.py
+++ static/py/app-weather_jeff.py
@@ -10,6 +10,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 
 
@@ -36,6 +37,7 @@ Base.prepare(engine, reflect=True)
 # Save reference to the table
 WeatherData = Base.classes.weather_data
 CityData = Base.classes.city_data
+StateData = Base.classes.state_data
 
 #################################################
 # Flask Setup
@@ -57,60 +59,7 @@ def welcome():
     )
 
 
-@app.route("/api/v1.0/weather/all_states")
-def all_states():
-    """Return a list of all weather for the state"""
-
-    # Query all passengers
-    session = Session(engine)
-    results = session.query(WeatherData).all()
-
-    # close the session to end the communication with the database
-    session.close()
-
-    # Convert list of tuples into normal list
-#     all_names = list(np.ravel(results))
-    all_data = []
-    for weather in results:
-        weather_dict = {}
-        weather_dict["state"] = weather.state
-        weather_dict["avg_high"] = weather.avg_high
-        weather_dict["avg_low"] = weather.avg_low
-        weather_dict["avg_prec"] = weather.avg_prec
-        weather_dict["month"] = weather.month
-        all_data.append(weather_dict)
-
-    return jsonify(all_data)
-
-#     return jsonify(all_names)
-
-
-# @app.route("/api/v1.0/passengers")
-# def passengers():
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-
-#     # Open a communication session with the database
-#     session = Session(engine)
-
-#     # Query all passengers
-#     results = session.query(Passenger).all()
-
-#     # close the session to end the communication with the database
-#     session.close()
-
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_passengers = []
-#     for passenger in results:
-#         passenger_dict = {}
-#         passenger_dict["name"] = passenger.name
-#         passenger_dict["age"] = passenger.age
-#         passenger_dict["sex"] = passenger.sex
-#         all_passengers.append(passenger_dict)
-
-#     return jsonify(all_passengers)
-
-
-@app.route("/api/v1.0/weather/census_city")
+@app.route("/api/v1.0/census_city")
 def census_city():
     """Return a list of all weather for the state"""
 
@@ -126,8 +75,9 @@ def census_city():
     census_city_data = []
 
     for city in results:
-        state_dict={'city_state': city.city_state}
+        city_dict={'city_state': city.city_state}
         data_dict={}
+        data_dict["state"] = city.state
         data_dict["median_income"] = city.median_income
         data_dict["mean_income"] = city.mean_income
         data_dict["median_value"] = city.median_value
@@ -148,7 +98,7 @@ def census_city():
         data_dict["bachelor_degree"] = city.bachelor_degree
         data_dict["graduate_proffesional_degree"] = city.graduate_proffesional_degree
         data_dict["high_school_graduate_or_higher"] = city.high_school_graduate_or_higher
-        data_dict["bachelor_degree_or_higher"] = city.bachelor_degress_or_higher
+        data_dict["bachelor_degree_or_higher"] = city.bachelor_degree_or_higher
         data_dict["white"] = city.white
         data_dict["black_african_american"] = city.black_african_american
         data_dict["american_indian_alaska_native"] = city.american_indian_alaska_native
@@ -157,10 +107,66 @@ def census_city():
         data_dict["hispanic_latino"] = city.hispanic_latino
 
         
-        state_dict['data'] = data_dict
-        census_city_data.append(state_dict)
+        city_dict['data'] = data_dict
+        census_city_data.append(city_dict)
 
     return jsonify(census_city_data)
+
+
+@app.route("/api/v1.0/census_state")
+def census_state():
+    """Return a list of all weather for the state"""
+
+    # Query all passengers
+    session = Session(engine)
+    results = session.query(StateData).all()
+
+    # close the session to end the communication with the database
+    session.close()
+
+    # Convert list of tuples into normal list
+#     all_names = list(np.ravel(results))
+    census_state_data = []
+
+    for state in results:
+        state_dict={'state': state.state}
+        data_dict={}
+        data_dict["median_income"] = state.median_income
+        data_dict["mean_income"] = state.mean_income
+        data_dict["median_value"] = state.median_value
+        data_dict["unemployment_rate"] = state.unemployment_rate
+        data_dict["population"] = state.population
+        data_dict["private_auto"] = state.private_auto 
+        data_dict["public_transport"] = state.public_transport 
+        data_dict["walks"] = state.walks 
+        data_dict["bike"] = state.bike
+        data_dict["other"] = state.other 
+        data_dict["works_home"] = state.works_home
+        data_dict["total_population_over_25_years_old"] = state.total_population_over_25_years_old
+        data_dict["less_than_9th_grade"] = state.less_than_9th_grade
+        data_dict["grade_9th_to_12th_no_diploma"] = state.grade_9th_to_12th_no_diploma
+        data_dict["high_school_diploma"] = state.high_school_diploma
+        data_dict["some_college_no_degree"] = state.some_college_no_degree
+        data_dict["associate_degree"] = state.associate_degree
+        data_dict["bachelor_degree"] = state.bachelor_degree
+        data_dict["graduate_proffesional_degree"] = state.graduate_proffesional_degree
+        data_dict["high_school_graduate_or_higher"] = state.high_school_graduate_or_higher
+        data_dict["bachelor_degree_or_higher"] = state.bachelor_degree_or_higher
+        data_dict["white"] = state.white
+        data_dict["black_african_american"] = state.black_african_american
+        data_dict["american_indian_alaska_native"] = state.american_indian_alaska_native
+        data_dict["asian"] = state.asian
+        data_dict["some_other_race"] = state.some_other_race
+        data_dict["hispanic_latino"] = state.hispanic_latino
+
+        
+        state_dict['data'] = data_dict
+        census_state_data.append(state_dict)
+
+    return jsonify(census_state_data)
+
+
+
 
 
 
